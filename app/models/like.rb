4cd1437fb2ec10e704e0efce07ada5d9177da13a
keisukekimura <keisuke.kimura@kikagaku.co.jp>
2020-09-19 14:15:14 +0900

    rails g controller コマンドで作成されるファイルえお制限

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :travel
+  validates :user_id, presence: true
+  validates :travel_id, presence: true, uniqueness: { scope: :user_id }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,12 @@
 # Travel.destroy_all
 # Like.destroy_all
 
-user1 = User.create!(email: "kimura@example.com", password: "password")
-user2 = User.create!(email: "kakesan@example.com", password: "password")
-user3 = User.create!(email: "taichisan@example.com", password: "password")
-user4= User.create!(email: "jinbosan@example.com", password: "password")
+# user1 = User.create!(email: "kimura@example.com", password: "password")
+# user2 = User.create!(email: "takesan@example.com", password: "password")
+# user3 = User.create!(email: "taichisan@example.com", password: "password")
+# user4= User.create!(email: "jinbosan@example.com", password: "password")
 
-Travel.create!(country: "スウェーデン", location: "ストックホルム", image: "", content: "ストックホルムにあるガムラスタンの街並みが気に入っていて訪れた時は必ず行きます。", user_id: user1.id)
-Travel.create!(country: "スウェーデン", location: "ストックホルム", image: "", content: "ストックホルムにあるガムラスタンの街並みが気に入っていて訪れた時は必ず行きます。", user_id: user1.id)
+# Travel.create!(country: "スウェーデン", location: "ストックホルム", image: "", content: "ストックホルムの旧市街でガムラスタンの街並みが気に入っています。魔女の宅急便のモデルにもなった街です。スウェーデンに訪れた時は必ず行きます。", user_id: user1.id)
+# Travel.create!(country: "日本", location: "沖縄", image: "", content: "沖縄にある最強食堂という定食屋さんの唐揚げ定食が好きです。", user_id: user2.id)
+
+# puts "初期データの投入に成功しました。"
