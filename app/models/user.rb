eed43a4fb10ff51fd44e6c1b8e5ac1c8ecf93160
keisukekimura <keisuke.kimura@kikagaku.co.jp>
2020-09-23 15:05:14 +0900

    show.html.erb に項目を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :travels, dependent: :destroy
+  has_many :favolites
+  has_many :travel_details
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
