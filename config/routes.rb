b3cdd01d695b7d83c885bd9f772ba38584ad3280
keisukekimura <keisuke.kimura@kikagaku.co.jp>
2020-09-23 22:41:53 +0900

    GoogleMapApiを導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root "travels#index"
   devise_for :users
   resources :travels
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
