e5e80a073303ea04788ba6133d116f2a15ef4319
keisukekimura <keisuke.kimura@kikagaku.co.jp>
2020-10-06 17:33:19 +0900

    heic2anyというラッパーのようなライブラリを入れてHEIC形式の画像をpngやjpegに変換して表示できるようにした

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,13 +21,35 @@ require("@fortawesome/fontawesome-free/js/all");
 require("./jquery.ezdz.min");
 
 document.addEventListener('DOMContentLoaded', () => {
+  const heicChanger = (target) => {
+    target.on('change', e => {
+      const blob = e.target.files[0]
+      if (blob.extension === "heic") {
+        e.target.classList.add('heic-change')
+        heic2any({
+          blob,
+          toType: "image/jpeg",
+          quality: 0.5
+        }).then(resultBlob => {
+          imageUrl = URL.createObjectURL(resultBlob)
+          $('.heic-change').ezdz('preview', imageUrl);
+          e.target.classList.remove('heic-change')
+        });
+      }
+    })
+  }
+
   $('input[type="file"]').each(function () {
     const imageUrl = $(this).data('image-url')
     $(this).ezdz()
     $(this).ezdz('preview', imageUrl);
   })
 
+  heicChanger($('input[type="file"]'))
+
   $('#travel-form').on('cocoon:after-insert', function (e, insertedItem, originalEvent) {
-    insertedItem.find('input[type="file"]').ezdz();
+    const insertedForm = insertedItem.find('input[type="file"]')
+    insertedForm.ezdz();
+    heicChanger(insertedForm)
   });
 });
