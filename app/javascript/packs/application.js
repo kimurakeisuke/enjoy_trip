c7ca6bd9857ae60239d6d55cca169c1687bd1819
keisukekimura <keisuke.kimura@kikagaku.co.jp>
2020-10-19 13:43:04 +0900

    slick.jsのコードを以前修正した内容よりもリファクタリングして動作を確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,57 +1,63 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start();
 // require("turbolinks").start()
 require("@rails/activestorage").start();
 require("channels");
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
 require("bootstrap/dist/js/bootstrap");
 require("@nathanvda/cocoon");
 require("@fortawesome/fontawesome-free/js/all");
 require("./jquery.ezdz.min");
-//= require jquery
-//= require rails-ujs
+require("slick-carousel/slick/slick.min");
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const heicChanger = (target) => {
-    target.on('change', e => {
-      const blob = e.target.files[0]
+    target.on("change", (e) => {
+      const blob = e.target.files[0];
       if (blob.extension === "heic") {
-        e.target.classList.add('heic-change')
+        e.target.classList.add("heic-change");
         heic2any({
           blob,
           toType: "image/jpeg",
-          quality: 0.5
-        }).then(resultBlob => {
-          imageUrl = URL.createObjectURL(resultBlob)
-          $('.heic-change').ezdz('preview', imageUrl);
-          e.target.classList.remove('heic-change')
+          quality: 0.5,
+        }).then((resultBlob) => {
+          imageUrl = URL.createObjectURL(resultBlob);
+          $(".heic-change").ezdz("preview", imageUrl);
+          e.target.classList.remove("heic-change");
         });
       }
-    })
-  }
+    });
+  };
 
   $('input[type="file"]').each(function () {
-    const imageUrl = $(this).data('image-url')
-    $(this).ezdz()
-    $(this).ezdz('preview', imageUrl);
-  })
+    const imageUrl = $(this).data("image-url");
+    $(this).ezdz();
+    $(this).ezdz("preview", imageUrl);
+  });
 
-  heicChanger($('input[type="file"]'))
+  heicChanger($('input[type="file"]'));
 
-  $('#travel-form').on('cocoon:after-insert', function (e, insertedItem, originalEvent) {
-    const insertedForm = insertedItem.find('input[type="file"]')
+  $("#travel-form").on("cocoon:after-insert", function (
+    e,
+    insertedItem,
+    originalEvent
+  ) {
+    const insertedForm = insertedItem.find('input[type="file"]');
     insertedForm.ezdz();
-    heicChanger(insertedForm)
+    heicChanger(insertedForm);
+  });
+
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: "",
+    nextArrow: "",
+    dotsClass: "slide-dots",
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
   });
 });
